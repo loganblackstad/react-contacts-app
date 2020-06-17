8f9ea83dfa9bc38feea230245f735e056ca59556
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-17 11:08:55 -0400

    added multi use generation and delete functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,17 +7,10 @@ export default class Contact extends Component {
   constructor(props) {
     super(props);
 
+    console.log(props.contact);
+
     this.state = {
       hidden: true,
-      image: '../logo.svg',
-      name: 'Hooman',
-      username: props.contact.login.username,
-      phone: props.contact.phone,
-      email: 'hooman@dog.com',
-      address: '',
-      city: '',
-      state: '',
-      zip: '',
     }
   }
 
@@ -26,19 +19,16 @@ export default class Contact extends Component {
   }
 
   deleteContact = () => {
-    this.props.dContact(this.state.username);
-    console.log(this.state.username);
+    this.props.dContact(this.props.contact.login.username);
+    console.log(this.props.contact.login.username);
   }
 
   editContact = () => { };
 
-
-
-
   render() {
 
     var divStyle = {
-      width: '100px',
+      width: '200px',
       marginLeft: '5px',
       border: '1px solid darkblue',
     };
@@ -64,23 +54,19 @@ export default class Contact extends Component {
         </Row>
         <Row className="justify-content-md-center sandbox">
           <Col xs={2} sm={2} md={2} lg={2} className="sandbox" style={divStyle}><p>name:</p></Col>
-          <Col xs={2} sm={2} md={2} lg={2} className="sandbox">{this.state.name}</Col>
+          <Col xs={2} sm={2} md={2} lg={2} className="sandbox">{this.props.contact.name.first}</Col>
         </Row>
-        <Row className="justify-content-md-center">
-          <Col style={boxy} xs lg="2"><p>email:</p></Col>
-          <Col style={boxy} md="auto">{this.state.email}</Col>
+        <Row className="justify-content-md-center sandbox">
+          <Col xs={2} sm={2} md={2} lg={2} className="sandbox" style={divStyle}><p>username:</p></Col>
+          <Col xs={2} sm={2} md={2} lg={2} className="sandbox">{this.props.contact.login.username}</Col>
         </Row>
-        <Row className="justify-content-md-center">
-          <Col style={boxy} xs lg="2"><p>phone:</p></Col>
-          <Col style={boxy} xs lg="1">{this.state.phone}</Col>
+        <Row className="justify-content-md-center sandbox">
+          <Col xs={2} sm={2} md={2} lg={2} className="sandbox" style={divStyle}><p>email:</p></Col>
+          <Col xs={2} sm={2} md={2} lg={2} className="sandbox">{this.props.contact.email}</Col>
         </Row>
-        <Row className="flex">
-          <Col style={boxy} xs={12} md={8}>xs=12 md=8</Col>
-          <Col style={boxy} xs={6} md={4}>xs=6 md=4</Col>
-        </Row>
-        <Row>
-          <Col style={boxy} md={4}>md=4</Col>
-          <Col style={boxy} md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
+        <Row className="justify-content-md-center sandbox">
+          <Col xs={2} sm={2} md={2} lg={2} className="sandbox" style={divStyle}><p>phone:</p></Col>
+          <Col xs={2} sm={2} md={2} lg={2} className="sandbox">{this.props.contact.phone}</Col>
         </Row>
       </Container >
     )
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './ContactList.css';
 import Contact from './Contact.jsx'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Button, Col, Row, Form, Container } from 'react-bootstrap'
 
 
 export default class ContactList extends Component {
@@ -12,27 +14,36 @@ export default class ContactList extends Component {
     }
   }
 
-
   deleteContact = (userName) => {
     const updatedContactList = this.state.contacts.filter((contact) => { return contact.login.username !== userName });
     this.setState({ contacts: updatedContactList })
   }
 
-
-  componentDidMount() {
+  generateContact = () => {
     fetch('https://randomuser.me/api/?results=1')
       .then(response => response.json())
       .then(data => {
-        this.setState({ contacts: data.results ? data.results : [] });
-        console.log(data.results[0]);
+        const newContacts = [...this.state.contacts, data.results[0]];
+        this.setState({ contacts: newContacts });
+        // console.log(data.results[0]);
       })
+  }
+
+  savetoLocalStorage
+
+  componentDidMount() {
+    this.generateContact();
+
   }
 
   render() {
     return (
-      <div className="ContactList" >
-        {this.state.contacts.map((contact, index) => <Contact contact={contact} key={index} dContact={this.deleteContact.bind(this)} />)}
-      </div>
+      <>
+        <Button type="button" onClick={this.generateContact}>Generate User</Button>
+        <div className="ContactList" >
+          {this.state.contacts.map((contact, index) => <Contact contact={contact} key={index} dContact={this.deleteContact.bind(this)} />)}
+        </div>
+      </>
     );
   }
 }
